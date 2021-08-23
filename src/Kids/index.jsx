8fc996301c0d37ba14b90942c980b7514dbee18c
raymondzeng28 <rzeng@cs.toronto.edu>
2021-08-23 15:50:10 -0700

    added Kids pictures

--- src/Kids/index.jsx
+++ src/Kids/index.jsx
@@ -8,20 +8,84 @@ class Kids extends React.Component {
     this.state = {
       pictures1: [
         {
-          orientation: `portrait`,
-          source: `Images/Test/bamboo_and_boats.jpg`,
-          height: 550,
-          width: null,
-          title: `Bamboo and Boats`,
-          description: `Artist: Xiangming Zeng`
+          orientation: `landscape`,
+          source: `Images/Kids/RyanLiuBull.jpg`,
+          height: null,
+          width: 630,
+          title: `Bull`,
+          description: `Artist: Ryan Liu`
         },
         {
           orientation: `landscape`,
-          source: `Images/Test/bamboo_and_cranes.jpg`,
+          source: `Images/Kids/KexinOwls.jpg`,
           height: null,
-          width: 650,
-          title: `Bamboo and Cranes`,
-          description: `Artist: Xiangming Zeng`
+          width: 630,
+          title: `Owls`,
+          description: `Artist: Kexin Wang`
+        },
+        {
+          orientation: `landscape`,
+          source: `Images/Kids/RachelLiuPelicans.jpg`,
+          height: null,
+          width: 630,
+          title: `Pelicans`,
+          description: `Artist: Rachel Liu`
+        },
+        {
+          orientation: `landscape`,
+          source: `Images/Kids/LucasZhangKoala.jpg`,
+          height: null,
+          width: 630,
+          title: `Koala`,
+          description: `Artist: Lucas Zhang`
+        },
+        {
+          orientation: `landscape`,
+          source: `Images/Kids/WesleyFish.png`,
+          height: null,
+          width: 630,
+          title: `Fish`,
+          description: `Artist: Wesley Xu`
+        },
+        {
+          orientation: `landscape`,
+          source: `Images/Kids/EmmaGaoWalrus.jpg`,
+          height: null,
+          width: 630,
+          title: `Walrus`,
+          description: `Artist: Emma Gao`
+        },
+        {
+          orientation: `landscape`,
+          source: `Images/Kids/chenxing_fish.jpg`,
+          height: null,
+          width: 630,
+          title: `Fish`,
+          description: `Artist: Chenxing Wang`
+        },
+        {
+          orientation: `landscape`,
+          source: `Images/Kids/TonyGemsbok.png`,
+          height: null,
+          width: 630,
+          title: `Gemsbok`,
+          description: `Artist: Tony Li`
+        },
+        {
+          orientation: `landscape`,
+          source: `Images/Kids/Zhongyu_Seal.jpg`,
+          height: null,
+          width: 630,
+          title: `Seal`,
+          description: `Artist: Zhongyu Wang`
+        },
+        {
+          orientation: `landscape`,
+          source: `Images/Kids/RyanZhangRiverOtter.jpg`,
+          height: null,
+          width: 630,
+          title: `River Otter`,
+          description: `Artist: Ryan Zhang`
         }
       ]
     };
--- src/Shared/Footer.jsx
+++ src/Shared/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Footer extends React.Component {
   render() {
-    return <p className="footer">&copy; International Pencil Drawing Competition 2021</p>
+    return <p className="footer">&copy; International Pencil Drawing Exhibition 2021</p>
   }
 }
 
